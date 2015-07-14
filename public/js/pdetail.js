645caa787128114729f995aa13cc8dc0a7508e91
Levy <zly.george@163.com>
2015-07-14 23:21:39 +0800

    little refine

--- public/js/pdetail.js
+++ public/js/pdetail.js
@@ -10,5 +10,12 @@ function procDocs()
         "&nbsp;&nbsp;&nbsp;&nbsp;"+
         "<span class='inlineblock'>"+formatDate(p_detail.pubtime)+"</span>"
     ));
+    if (p_detail.tag.length>0)
+    {
+        var tagstr="";
+        for (var i=0;i<p_detail.tag.length;i++)
+            tagstr+=(i==0?"":",&nbsp;&nbsp;")+p_detail.tag[i];
+        $("#lafin").before($("<p class='byline'>").html("<span class='icon-price-tags'></span>&nbsp;&nbsp;Tags:&nbsp;&nbsp;"+tagstr));
+    }
     $(".tem_cata").text(p_detail.catalog);
 }
