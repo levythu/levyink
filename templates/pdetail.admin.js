41deae10bd955f7177a812fc51473faa615d4dd5
Levy <zly.george@163.com>
2016-02-26 16:09:17 +0800

    refine

--- templates/pdetail.admin.js
+++ templates/pdetail.admin.js
@@ -7,5 +7,5 @@ function called_on_loaded()
 {
     var t=$("#blogCont > h1");
     if (t.length>0)
-        t[0].innerHTML+="&nbsp;&nbsp;<a href=\""+pdetail_admin_js.editor_url+p_detail.pid+"\">"+"(EDIT)</a>";
+        t[0].innerHTML+="&nbsp;&nbsp;<span class='editButton'><a href=\""+pdetail_admin_js.editor_url+p_detail.pid+"\">"+"(EDIT)</a></span>";
 }
