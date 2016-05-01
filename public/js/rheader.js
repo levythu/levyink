7118ca32231a283a77c40a3bf53fecb8b23b1f88
Levy <zly.george@163.com>
2016-05-01 16:10:34 +0800

    one big step to eleminate all the size-adjusting codes in js

--- public/js/rheader.js
+++ public/js/rheader.js
@@ -5,18 +5,6 @@ var blog_js=
 
 $(document).ready(function()
 {
-    $("#topInk").css("width","1366px");
-    blog_js.titleTrigger1=
-        $("#latitle")[0].offsetLeft+$("#bigtitle")[0].offsetLeft+$("#bigtitle")[0].offsetWidth
-        +trimStringToNum($("#direct-panel").css("width"))
-        +trimStringToNum($("#direct-panel").css("right"));
-    blog_js.latitle_normal=trimStringToNum($("#latitle").css("bottom"));
-    blog_js.latitle_elevated=blog_js.latitle_normal+$("#direct-panel")[0].offsetHeight+20;
-    blog_js.bigtitle=trimStringToNum($("#bigtitle").css("font-size"));
-    blog_js.smalltitle=trimStringToNum($("#smalltitle").css("font-size"));
-    $("#topInk").css("width","100%");
-
-    $(window).resize(updateLayout);
     $("body").dblclick(function(e)
     {
         scroll2Top();
@@ -45,64 +33,12 @@ $(document).ready(function()
     $(window).trigger("resize");
     $("html").load(function(){
         $(window).trigger("resize");
-        setTimeout(function(){$(window).trigger("resize");},2000);
     });
     setInterval(function(){$(window).trigger("resize");},2000);
 
     if (getParameterByName('toTop')!="")
         setTimeout(function(){scroll2Top()}, 500);
 });
-
-function updateLayout()
-{
-    topInk_onResize();
-}
-function topInk_onResize()
-{
-    var topInkH2W=1008/3072;
-    var contentWidth=$("#topInk")[0].offsetWidth;
-
-    if ($("#topInk")[0].offsetWidth>=blog_js.titleTrigger1)
-    {
-        //$("#giPic").css("width","100%");
-        $("#latitle").css("bottom",blog_js.latitle_normal)
-                     .css("text-align","left");
-        $("#direct-panel").css("right","20px");
-    }
-    else
-    {
-        $("#direct-panel").css("right",(contentWidth-$("#direct-panel")[0].offsetWidth)/2);
-        contentWidth=blog_js.titleTrigger1;
-        //$("#giPic").css("width",contentWidth);
-        $("#latitle").css("bottom",blog_js.latitle_elevated)
-                     .css("text-align","center");
-    }
-    $("#topInk").css("height",contentWidth*topInkH2W);
-    $("#followedFrame").css("top",contentWidth*topInkH2W+50);
-
-    //adjust fontsize dynamically
-    {
-        var isShrink=false;
-        var fontsize=trimStringToNum($("#bigtitle").css("font-size"));
-        while ($("#bigtitle")[0].offsetWidth>$("#latitle")[0].offsetWidth)
-        {
-            isShrink=true;
-            fontsize--;
-            $("#bigtitle").css("font-size",fontsize);
-            $("#smalltitle").css("font-size",0);
-        }
-        if (!isShrink)
-        {
-            while (fontsize<blog_js["bigtitle"] && $("#bigtitle")[0].offsetWidth<$("#latitle")[0].offsetWidth)
-            {
-                fontsize++;
-                $("#bigtitle").css("font-size",fontsize);
-                if (fontsize==blog_js["bigtitle"] && $("#bigtitle")[0])
-                    $("#smalltitle").css("font-size",blog_js.smalltitle);
-            }
-        }
-    }
-}
 function scroll2Top(disableAnime)
 {
     if (disableAnime===true)
