fee9dc3b5292aa7781c88b6f1d80cb07d72ec330
Levy <zly.george@163.com>
2016-06-17 16:39:53 +0800

    no message

--- routes/outauth/askforauth.js
+++ routes/outauth/askforauth.js
@@ -9,11 +9,11 @@ var protocolInfo=require("../../models/protocolDeclare");
 
 var storage=require("./authstorage");
 
-router.post("/ask", function(req, res) {
+router.get("/ask", function(req, res) {
     var token=storage.CreateAuth();
     if (token!="") {
         try {
-            var des=url.parse(req.body.destination);
+            var des=url.parse(decodeURIComponent(req.query.destination));
             if (des.search==undefined) des.search="?token="+token;
             else des.search+="&token="+token;
             res.redirect(url.format(des));
