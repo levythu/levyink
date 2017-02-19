adfb4523f4a5f84477b8353de71e4210eb0a5761
Levy <zly.george@163.com>
2017-02-19 18:38:54 -0500

    update special list api

--- routes/blogs/blogmanage.js
+++ routes/blogs/blogmanage.js
@@ -10,6 +10,56 @@ var db=model.db;
 
 var searchable=["catalog","author"];
 
+function digestAll(req, res) {
+    var searchCondition={};
+    var us=req.session.author || "%IMPOSSIBLE%";
+
+    db[model.AUTHOR].find(
+    {
+        name: us
+    },function(err,docs)
+    {
+        if (err||docs.length==0)
+        {
+            searchCondition["order"]={$gt:-1};
+        }
+        if (searchable.indexOf(req.query.filter)>=0)
+        {
+            searchCondition[req.query.filter]=req.query.key;
+        }
+        db[model.BLOG].find(searchCondition,{content:0, preview:0})
+            .sort({order:-1,pubtime:-1}).toArray(function(err, doc) {
+            if (err) {
+                res.send(JSON.stringify({
+                    "status": protocolInfo.generalRes.statusCode.DB_ERROR,
+                    "content": null
+                }));
+                return;
+            }
+            var artList=[];
+            for (var i=0;i<doc.length;i++) {
+                artList.push(protocolInfo.secure(
+                {
+                    title: doc[i].title,
+                    catalog: doc[i].catalog,
+                    pubtime: doc[i].pubtime,
+                    pid: doc[i].pid,
+                    order: doc[i].order,
+                    url: puburl.blogpage.replace(/%PID%/g,doc[i].pid)
+                }));
+            }
+            res.send(JSON.stringify({
+                "status": protocolInfo.generalRes.statusCode.NORMAL,
+                "content": [],
+                "blog_in_total": artList.length,
+                "digest_content": artList,
+                "reserved": "[置顶][隐藏]"
+            }));
+            return;
+        }
+    });
+}
+
 router.get('/list', function(req, res)
 {
     if (req.query.fetchstart==undefined || req.query.fetchcount==undefined || req.query.filter==undefined)
@@ -22,6 +72,11 @@ router.get('/list', function(req, res)
     }
     var fstart=(-(-req.query.fetchstart));
     var fcount=(-(-req.query.fetchcount));
+    if (fstart<0) {
+        // special logic: return a empty result but with all digest
+        digestAll(req, res);
+        return;
+    }
     if (fstart==NaN || fcount==NaN || fstart<0 || fcount<0)
     {
         res.send(JSON.stringify({
